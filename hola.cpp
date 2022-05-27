4ae5b1c18810bea558baac68268e8993e4f032f6
aguilator69 <106150778+aguilator69@users.noreply.github.com>
2022-05-27 09:15:40 +0200

    Add files via upload

---
+++ hola.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+int main(void)
+{
+    const float PI = 3.1416;
+    float radio;
+    cout << "Introduce el valor del radio : ";
+    cin >> radio;
+    cout << "\nLongitud de la circunferencia: " << fixed << setprecision(2) << 2*PI*radio;
+    cout << "\nArea del circulo: " << PI*pow(radio,2);
+    cout << "\nVolumen de la esfera: " << (4.0/3)*PI*pow(radio,3) << endl;
+    system("pause");
+}
